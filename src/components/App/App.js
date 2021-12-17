1af8f1a876dea0efaed67af334c21cdacb6e7049
Geraldo Moura <geraldo.moura@ymail.com>
2021-12-17 14:37:07 -0600

    changing local variables + signup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { initializeApp } from "firebase/app";
-import "firebase/auth"
-
-import Landing from '../../views/Landing';
+import { AuthProvider } from '../../services/AuthContext';
+// import { initializeApp } from "firebase/app";
+// import Landing from '../../views/Landing';
+import Signup from '../Auth/Signup';
 // import logo from '../../assets/logo.svg';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBAsoMYxzM46yavU-4Em-1ihbBNWzznvDg",
-  authDomain: "blueocean-612b2.firebaseapp.com",
-  projectId: "blueocean-612b2",
-  storageBucket: "blueocean-612b2.appspot.com",
-  messagingSenderId: "43711589559",
-  appId: "1:43711589559:web:effcb703d3262364feb132",
-  measurementId: "G-TW42PT564X"
-};
-
 const App = () => {
-  initializeApp(firebaseConfig);
-
-
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 };
 
