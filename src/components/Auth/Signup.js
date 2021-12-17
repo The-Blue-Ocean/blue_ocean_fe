6feea26007c03fa66f30d2996b5cf02a5e838dfa
Geraldo Moura <geraldo.moura@ymail.com>
2021-12-17 12:31:15 -0600

    auth initial

---
+++ src/components/Auth/Signup.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Signup() {
+    return (
+        <>
+            <div className='login-container'>
+                <h2>Sign Up</h2>
+                <form action="">
+
+                </form>
+            </div>
+            <div className=''>
+                Already have an account? Log In
+            </div>
+        </>
+    )
+}
