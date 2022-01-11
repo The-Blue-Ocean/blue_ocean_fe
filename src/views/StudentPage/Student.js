52375261a5f9a3ee73283edc051fa4186e16f42e
Geraldo Moura <geraldo.moura@ymail.com>
2022-01-11 13:38:46 -0600

    created private route to students page

--- src/views/StudentPage/Student.js
+++ src/views/StudentPage/Student.js
@@ -1,33 +1,34 @@
-import React, { useEffect , useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Student.css";
 import axios from "axios";
-import Nav from '../../components/LeftNavBar/nav'
+// import Nav from '../../components/LeftNavBar/nav'
+
 export const Student = (props) => {
-    const [studentData, setStudentData] = useState({})
-    
-    useEffect(() => {
-        axios
-        .get(`https://blue-ocean-be.uc.r.appspot.com/api/students/${props.student}`)
-        .then((response) => {
+  const [studentData, setStudentData] = useState({})
+
+  useEffect(() => {
+    axios
+      .get(`https://blue-ocean-be.uc.r.appspot.com/api/students/${props.student}`)
+      .then((response) => {
         setStudentData(response.data[0])
-        
-        });
-        
-    },[])
-    
-    
- //console.log(studentData[0]['name'])
- //studentData[0]['name']
-//studentData[0]['rank']
-    return (
+
+      });
+
+  }, [])
+
+
+  //console.log(studentData[0]['name'])
+  //studentData[0]['name']
+  //studentData[0]['rank']
+  return (
     <>
-      
+
       <div className="studentcontainer">
-          <div>{studentData.name}</div>
-          <div>{studentData.rank}</div>
-          <div>{studentData.ets}</div>
-          <div>{studentData.cohort}</div>
-          <div>{}</div>
+        <div>{studentData.name}</div>
+        <div>{studentData.rank}</div>
+        <div>{studentData.ets}</div>
+        <div>{studentData.cohort}</div>
+        <div>{ }</div>
       </div>
     </>
   );
