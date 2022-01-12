ccaed441bc8a7c48f14e73b4b01e7e71a15f5fbd
Geraldo Moura <geraldo.moura@ymail.com>
2022-01-12 13:19:27 -0600

    testing local backend

--- src/services/fetchAPI.js
+++ src/services/fetchAPI.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const url = 'http://localhost/api/admin'
-const url = 'https://blue-ocean-be.uc.r.appspot.com/api/students'
+// const url = 'https://blue-ocean-be.uc.r.appspot.com/api/students'
+const url = 'https://localhost/api/students'
 // const urlAdmin = 'http://localhost/api/adminEmail'
 
 export const fetchData = async (token, userRole) => {
