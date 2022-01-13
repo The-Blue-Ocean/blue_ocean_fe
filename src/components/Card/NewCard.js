e54ed7cfa39f09ffe0eb62236dd0ece5e79f251a
Geraldo Moura <geraldo.moura@ymail.com>
2022-01-13 17:32:55 -0600

    fixing data fetches

--- src/components/Card/NewCard.js
+++ src/components/Card/NewCard.js
@@ -4,6 +4,12 @@ import { useNavigate } from "react-router-dom";
 const Card = (props) => {
   const navigate = useNavigate()
 
+  const handleClickStudent = (e) => {
+    console.log(e.target.value)
+    // student._id(e.target.value)
+    navigate('/student')
+  }
+
   return (
     <div className="card">
       <p className="time-period">{props.timePeriod}</p>
@@ -11,10 +17,7 @@ const Card = (props) => {
       <div className="studentcontainer">
         <div>
           {props.students.map((student) => {
-            return <button key={student._id} value={student._id} className="student" onClick={(e) => {
-              // props.students._id(e.target.value)
-              navigate('/student')
-            }}> - {student.name}</button>;
+            return <button key={student._id} value={student._id} className="student" onClick={handleClickStudent}>{student.name}</button>;
           })}
         </div>
       </div>
--- src/views/StudentPage/Student.js
+++ src/views/StudentPage/Student.js
@@ -6,15 +6,15 @@ import axios from "axios";
 export const Student = (props) => {
   const [studentData, setStudentData] = useState({})
 
-  useEffect(() => {
-    axios
-      .get(`https://blue-ocean-be.uc.r.appspot.com/api/students/${props.student}`)
-      .then((response) => {
-        setStudentData(response.data[0])
+  // useEffect(() => {
+  //   axios
+  //     .get(`https://blue-ocean-be.uc.r.appspot.com/api/students/${props.student}`)
+  //     .then((response) => {
+  //       setStudentData(response.data[0])
 
-      });
+  //     });
 
-  }, [props.student])
+  // }, [props.student])
 
 
   //console.log(studentData[0]['name'])
@@ -22,13 +22,12 @@ export const Student = (props) => {
   //studentData[0]['rank']
   return (
     <>
-
       <div className="studentcontainer">
         <div>{studentData.name}</div>
         <div>{studentData.rank}</div>
         <div>{studentData.ets}</div>
         <div>{studentData.cohort}</div>
-        <div>{ }</div>
+        <div>Test</div>
       </div>
     </>
   );
